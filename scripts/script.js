b7c4b2ec1ad8ed41d075fdf937df9f0943600edd
wasim altayyan <wasimtayyan@gmail.com>
2023-05-12 22:28:03 +0300

    first step

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+const word = []
+let maxLives = 10
+const gameMessage = document.querySelector('#message')
+async function getRandomWord() {
+    const resp = await fetch("https://random-word-api.herokuapp.com/word?number=1")
+    const array = await resp.json()
+    word.push(array[0])
+    console.log(word)
+    wordLength(word)
+    replacLitters(word)
+    // checkletters(word)
+}
+function wordLength(word) {
+    let length = word.length
+    console.log(length)
+}
+
+const blank = document.querySelector("#blanks")
+const pargraph = document.createElement("p")
+const blankInd = []
+function replacLitters(word) {
+
+    for (let i = 0; i < word[0].length; i++) {
+
+
+        blankInd[i] = ' _ '
+    }
+    pargraph.textContent += blankInd.join('')
+    blank.appendChild(pargraph)
+}
+console.log(blankInd)
+const btns = document.querySelectorAll(".btn")
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        btn.classList.toggle('clicked')
+        let value = btn.getAttribute('value')
+        checkletters(value)
+    })
+})
+
+function checkletters(value) {
+    let letter = word[0]
+    let indices = [];
+    for (let i = 0; i < letter.length; i++) {
+        if (letter[i] === value) {
+            indices.push(i);
+            blankInd[i] = value
+            pargraph.textContent = blankInd.join('')
+
+        }else {
+            maxLives --;
+            
+        }
+    }
+}
+getRandomWord()
